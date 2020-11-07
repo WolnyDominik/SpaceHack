48458208ee62a021fbd0f773774b9f0797690919
achillesv23 <achillesv23@gmail.com>
2020-11-07 21:14:21 +0100

    Textury + maszyny

--- pathNode.js
+++ pathNode.js
@@ -6,8 +6,9 @@ const pathType = {
 var tileSet = new Image();
 tileSet.src = "/src/textury.png";
 class PathNode {
-    constructor(type, index, callback) {
+    constructor(type, index, mIndex, callback) {
         this.index = index;
+        this.mIndex = mIndex;
         this.callback = callback;
         this.type = type || pathType.DEFAULT;
         this.color = "rgba("+(Math.random()*255)+","+(Math.random()*255)+","+(Math.random()*255)+",255)";
@@ -20,6 +21,7 @@ class PathNode {
             ctx.scale(-1,1);
         }
         ctx.drawImage(tileSet,(this.index%32)*64,Math.floor(this.index/64)*128,64,128,-32,-64,64,128);
+        ctx.drawImage(tileSet,(this.mIndex%32)*64,Math.floor(this.mIndex/64)*128,64,128,-32,-64,64,128);
         ctx.restore();
     }
 }
