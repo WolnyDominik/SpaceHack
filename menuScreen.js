3897af962031e057622c77f21eb724f49ae4925a
Havystar <31964929+Havystar@users.noreply.github.com>
2020-11-07 14:05:58 +0100

    optimalization of containers

--- menuScreen.js
+++ menuScreen.js
@@ -5,8 +5,12 @@ class MenuScreen extends Screen {
         this.image = new Image();
         this.image.src = "/src/img/background.png";
         this.menuBtns = new Array();
-        this.menuBtns.push(new Button(150, 150, 600, 55, "PLAY" , "rgba(70,100,168,255)" , "rgba(90,120,188,255)" , "rgba(240,128,0,255)" , "rgba(255,255,255,225", 40, ()=>{screenManager.pushScreen(gameScreen)}));
-        this.menuBtns.push(new Button(150, 300, 600, 55, "OPTIONS" , "rgba(70,100,168,255)" , "rgba(90,120,188,255)" , "rgba(240,128,0,255)" , "rgba(255,255,255,225", 40, ()=>{this.activateContainer(0)}));
+        this.menuBtns.push(new Button(150, 150, 600, 55, "PLAY", "rgba(70,100,168,255)", "rgba(90,120,188,255)", "rgba(240,128,0,255)", "rgba(255,255,255,225", 40, () => {
+            screenManager.pushScreen(gameScreen)
+        }));
+        this.menuBtns.push(new Button(150, 300, 600, 55, "OPTIONS", "rgba(70,100,168,255)", "rgba(90,120,188,255)", "rgba(240,128,0,255)", "rgba(255,255,255,225", 40, () => {
+            this.activateContainer(0)
+        }));
         this.focused = true;
 
         this.bckgrdAudio = new Audio("src/sound/title.mp3");
@@ -15,36 +19,35 @@ class MenuScreen extends Screen {
         this.bckgrdAudio.play();
         
         this.containers = new Array();
+        this.activeContainerId = 0;
         this.btns = new Array();
-        this.btns.push(new Button(0, 0, 100, 55, "OK" , "rgba(70,100,168,255)" , "rgba(90,120,188,255)" , "rgba(240,128,0,255)" , "rgba(255,255,255,225", 40, ()=>{this.focus()}));
-        this.containers.push(new Container(undefined, undefined, 500, 200, "rgba(255,100,255,255)", this.btns, ()=>{}));
+        this.btns.push(new Button(0, 0, 100, 55, "OK", "rgba(70,100,168,255)", "rgba(90,120,188,255)", "rgba(240,128,0,255)", "rgba(255,255,255,225", 40, () => {
+            this.focus()
+        }));
+        this.containers.push(new Container(undefined, undefined, 500, 200, "rgba(255,100,255,255)", this.btns, () => {}));
     }
 
-    onClick(x, y, buttons){
-        if(this.focused){
+    onClick(x, y, buttons) {
+        if (this.focused) {
             super.onClick(x, y, buttons);
             for (let j = 0; j < this.menuBtns.length; j++) {
                 this.menuBtns[j].onClick(x, y, buttons);
             }
         }
-        for (let j = 0; j < this.containers.length; j++) {
-            if (this.containers[j].active) {
-                this.containers[j].onClick(x, y, buttons);
-            }
+        if (this.containers[this.activeContainerId].active) {
+            this.containers[this.activeContainerId].onClick(x, y, buttons);
         }
     }
 
     onMove(x, y, buttons) {
         super.onMove(x, y);
-        if(this.focused){
+        if (this.focused) {
             for (let j = 0; j < this.menuBtns.length; j++) {
                 this.menuBtns[j].onMove(x, y);
             }
         }
-        for (let j = 0; j < this.containers.length; j++) {
-            if (this.containers[j].active) {
-                this.containers[j].onMove(x, y, buttons);
-            }
+        if (this.containers[this.activeContainerId].active) {
+            this.containers[this.activeContainerId].onMove(x, y, buttons);
         }
     }
 
@@ -55,48 +58,38 @@ class MenuScreen extends Screen {
         }
     }
 
-    update(){
+    update() {
         this.ticks++;
-        for (let j = 0; j < this.containers.length; j++) {
-            if (this.containers[j].active) {
-                this.containers[j].update();
-            }
+        if (this.containers[this.activeContainerId].active) {
+            this.containers[this.activeContainerId].update();
         }
     }
 
     activateContainer(i) {
-        console.log("options"); 
-        this.focused = false; 
+        console.log("options");
+        this.focused = false;
         this.containers[i].active = true;
+        this.activeContainerId = i;
     }
 
-    draw(){
+    draw() {
         //ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
         ctx.save();
-        
+
         ctx.save();
         ctx.clearRect(0, 0, canvas.width, canvas.height);
-        ctx.translate(canvas.width/2,canvas.height/2);
-        ctx.drawImage(this.image, 0, 0, this.image.width, this.image.height, -canvas.width/2, -canvas.height/2, canvas.width, canvas.height);
+        ctx.translate(canvas.width / 2, canvas.height / 2);
+        ctx.drawImage(this.image, 0, 0, this.image.width, this.image.height, -canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
         ctx.restore();
-        
+
         for (let j = 0; j < this.menuBtns.length; j++) {
             this.menuBtns[j].draw();
         }
-
-        for (let j = 0; j < this.containers.length; j++) {
-            if (this.containers[j].active) {
-                this.containers[j].draw();
-            }
+        if (this.containers[this.activeContainerId].active) {
+            this.containers[this.activeContainerId].draw();
         }
-        
-        ctx.restore();
-      
-    }
 
-    pause() {
-        super.pause()
-        this.bckgrdAudio.pause()
+        ctx.restore();
+
     }
 }
-
